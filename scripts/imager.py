55bb62414b0fe26cc0a9f6593e93009f8d4cbca8
Bruce Merry <bmerry@ska.ac.za>
2015-05-07 10:06:16 +0200

    FITS file writing
    It's a quick hack for testing: most of the parameters are hard-coded,
    and the values are all zero.

--- scripts/imager.py
+++ scripts/imager.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
+import math
 import argparse
 import casacore.tables
-import astropy.io.fits
+import astropy.io.fits as fits
+import astropy.wcs as wcs
+import numpy as np
 import katsdpsigproc
 import katsdpimager.loader
 from contextlib import closing
@@ -15,6 +18,35 @@ def _key_values_to_dict(items):
             raise ValueError('Missing equals sign in "{}"'.format(item))
         out[kv[0]] = kv[1]
     return out
+
+def write_fits(data, cellsizes, filename):
+    header = fits.Header()
+    header['BUNIT'] = 'JY/BEAM'
+    header['ORIGIN'] = 'katsdpimager'
+    # Transformation from pixel coordinates to intermediate world
+    # coordinates, which are taken to be l, m coordinates. The reference
+    # point is current taken to be the centre of the image.
+    # Note that astropy.io.fits reverses the axis order.
+    header['CRPIX1'] = data.shape[1] * 0.5 + 0.5
+    header['CRPIX2'] = data.shape[0] * 0.5 + 0.5
+    # FITS uses degrees; and RA increases right-to-left
+    header['CDELT1'] = -cellsizes[0] * 180.0 / math.pi
+    header['CDELT2'] = cellsizes[1] * 180.0 / math.pi
+
+    # Transformation from intermediate world coordinates to world
+    # coordinates (celestial coordinates in this case).
+    # TODO: get equinox from input
+    header['EQUINOX'] = 2000.0
+    header['RADESYS'] = 'FK5' # Julian equinox
+    header['CUNIT1'] = 'deg'
+    header['CUNIT2'] = 'deg'
+    header['CTYPE1'] = 'RA---SIN'
+    header['CTYPE2'] = 'DEC--SIN'
+    header['CRVAL1'] = 52.5 # RA of field centre
+    header['CRVAL2'] = -35.0 # DEC of field centre
+
+    hdu = fits.PrimaryHDU(data, header)
+    hdu.writeto(filename)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -32,6 +64,9 @@ def main():
         print(dataset.longest_baseline())
         for chunk in dataset.data_iter(args.channel, 65536):
             print(chunk['vis'].shape, chunk['weights'].shape, chunk['uvw'].shape)
+
+    image = np.zeros((1024, 1024), dtype=np.float32)
+    write_fits(image, (4.8481e-06, 4.8481e-06), args.output_file)
 
 if __name__ == '__main__':
     main()
