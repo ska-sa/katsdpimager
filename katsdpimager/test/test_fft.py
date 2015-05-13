bd58f9ea3fc5c3760d3f3bba3b12ace2f8426fc0
Bruce Merry <bmerry@ska.ac.za>
2015-05-13 15:46:56 +0200

    Add tests for FFT

--- katsdpimager/test/test_fft.py
+++ katsdpimager/test/test_fft.py
@@ -42,3 +42,35 @@ class TestFftshift(object):
         fn()
         actual = data.get(command_queue)
         np.testing.assert_equal(expected, actual)
+
+
+class TestFft(object):
+    @device_test
+    @cuda_test
+    def test_forward(self, context, command_queue):
+        rs = np.random.RandomState(1)
+        template = fft.FftTemplate(command_queue, 2, (16, 48, 3, 2), np.complex64, (24, 64, 4, 5), (20, 48, 4, 5))
+        fn = template.instantiate(fft.FFT_FORWARD, allocator=accel.SVMAllocator(context))
+        fn.ensure_all_bound()
+        src = fn.buffer('src')
+        dest = fn.buffer('dest')
+        src[:] = (rs.standard_normal(src.shape) + 1j * rs.standard_normal(src.shape)).astype(np.complex64)
+        fn()
+        command_queue.finish()
+        expected = np.fft.fftn(src, axes=(0, 1))
+        np.testing.assert_allclose(expected, dest, rtol=1e-4)
+
+    @device_test
+    @cuda_test
+    def test_inverse(self, context, command_queue):
+        rs = np.random.RandomState(1)
+        template = fft.FftTemplate(command_queue, 2, (16, 48, 3, 2), np.complex64, (24, 64, 4, 5), (20, 48, 4, 5))
+        fn = template.instantiate(fft.FFT_INVERSE, allocator=accel.SVMAllocator(context))
+        fn.ensure_all_bound()
+        src = fn.buffer('src')
+        dest = fn.buffer('dest')
+        src[:] = (rs.standard_normal(src.shape) + 1j * rs.standard_normal(src.shape)).astype(np.complex64)
+        fn()
+        command_queue.finish()
+        expected = np.fft.ifftn(src, axes=(0, 1)) * (src.shape[0] * src.shape[1])
+        np.testing.assert_allclose(expected, dest, rtol=1e-4)
