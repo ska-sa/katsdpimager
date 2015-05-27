05ea981db263b0898e483ca690abf0d80150af79
Bruce Merry <bmerry@ska.ac.za>
2015-05-27 11:39:48 +0200

    Write coverage as XML

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -4,4 +4,4 @@ pip install -U pip setuptools wheel
 pip install numpy  # Some requirements need it already installed
 pip install -r requirements.txt
 pip install coverage
-nosetests --with-coverage --cover-package=katsdpimager --cover-html
+nosetests --with-coverage --cover-package=katsdpimager --cover-xml
