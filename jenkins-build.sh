d4b31bb3e3a9ee4ac898a609d6bf030c32daa05b
Bruce Merry <bmerry@ska.ac.za>
2015-05-27 11:09:42 +0200

    Add Jenkins test script

---
+++ jenkins-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e -x
+pip install -U pip setuptools wheel
+pip install numpy  # Some requirements need it already installed
+pip install -r requirements.txt
+pip install coverage
+nosetests --with-coverage --cover-package=katsdpimager --cover-html
