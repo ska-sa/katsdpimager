b99509c9d0c7b8526a864f260535478f39005734
Bruce Merry <bmerry@ska.ac.za>
2015-05-27 16:53:44 +0200

    Add --with-xunit to get test reports on Jenkins

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -4,7 +4,7 @@ pip install -U pip setuptools wheel
 pip install numpy  # Some requirements need it already installed
 pip install -r requirements.txt
 pip install coverage
-nosetests --with-coverage --cover-erase --cover-package=katsdpimager --cover-xml
+nosetests --with-xunit --with-coverage --cover-erase --cover-package=katsdpimager --cover-xml
 # Hack to try to make Jenkins Cobertura plugin find the source
 sed -i -e 's!katsdppipelines/katsdpimager</source>!katsdppipelines</source>!' \
        -e 's!filename="katsdpimager/!filename="katsdpimager/katsdpimager/!g' \
