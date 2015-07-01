05ef01cb33d7e041e16d55bf9bf474491a73f7ee
Bruce Merry <bmerry@ska.ac.za>
2015-07-01 10:50:47 +0200

    Add module for preprocessing visibilities
    It's not hooked into anything yet.

---
+++ katsdpimager/preprocess.py
@@ -0,0 +1,401 @@
+# -*- coding: utf-8 -*-
+
+"""Pre-process visibilities into a form that is more suitable for gridding.
+
+The following transformations are done:
+
+- Autocorrelations are removed
+- Visibilities with w < 0 are flipped
+- UVW coordinates are converted to grid coordinates and quantized
+- Weights are multiplied into the visibilities (but are still retained)
+- Visibilities are sorted by channel then W slice
+- Visibilities are partially sorted by baseline, then time
+- Visibilities with the same UVW coordinates (after quantisation) are merged
+  ("compressed").
+
+The resulting visibilities are stored in HDF5. A separate dataset is created
+for each channel and W slice, because the data needs to be fully sorted by W
+slice. HDF5 variable-length arrays are not suitable because the whole
+variable-length array has to be loaded as a unit.
+
+Partial sorting by baseline is implemented by buffering up a reasonably large
+number of visibilities and sorting them. The merging is also partial, since
+only adjacent visibilities are candidates for merging.
+
+There is also a memory-based backend to simplify testing.
+"""
+
+from __future__ import print_function, division
+import h5py
+import numpy as np
+import katsdpimager.grid as grid
+import astropy.units as units
+
+
+def _make_dtype(num_polarizations, internal):
+    """Creates a numpy structured dtype to hold a preprocessed visibility with
+    associated metadata.
+
+    Parameters
+    ----------
+    num_polarizations : int
+        Number of polarizations in the visibility.
+    internal : bool
+        If True, the structure will include extra fields for baseline, w slice
+        and channel.
+    """
+    fields = [
+        ('uv', np.int16, (2,)),
+        ('sub_uv', np.int16, (2,)),
+        ('weights', np.float32, (num_polarizations,)),
+        ('vis', np.complex64, (num_polarizations,)),
+        ('w_plane', np.int16)
+    ]
+    if internal:
+        fields += [('baseline', np.int16), ('w_slice', np.int16), ('channel', np.int32)]
+    return np.dtype(fields)
+
+
+def _make_fapl(cache_entries, cache_size):
+    """Create a File Access Properties List for h5py with a specified number
+    of cache entries and cache size. This is based around the internal
+    make_fapl function in h5py.
+    """
+
+    fapl = h5py.h5p.create(h5py.h5p.FILE_ACCESS)
+    cache_settings = list(fapl.get_cache())
+    fapl.set_cache(cache_settings[0], cache_entries, cache_size, cache_settings[3])
+    fapl.set_fclose_degree(h5py.h5f.CLOSE_STRONG)
+    fapl.set_libver_bounds(h5py.h5f.LIBVER_LATEST, h5py.h5f.LIBVER_LATEST)
+    return fapl
+
+
+class VisibilityCollector(object):
+    """Base class that accepts a stream of visibility data and stores it. The
+    subclasses provide the storage backends.
+    """
+    def __init__(self, image_parameters, grid_parameters):
+        self.image_parameters = image_parameters
+        self.grid_parameters = grid_parameters
+        num_polarizations = len(image_parameters[0].polarizations)
+        self.store_dtype = _make_dtype(num_polarizations, False)
+        self.buffer_dtype = _make_dtype(num_polarizations, True)
+        # TODO: set buffer size more sensibly
+        self._buffer = np.rec.recarray((1024**2,), self.buffer_dtype)
+        #: Number of valid elements in buffer
+        self._used = 0
+
+    @property
+    def num_channels(self):
+        return len(self.image_parameters)
+
+    @property
+    def num_w_slices(self):
+        return 1
+
+    def _process_buffer(self):
+        """Sort and compress the buffer, and write the results to file.
+
+        The buffer is sorted, then compressed in-place. It is then split into
+        regions that have the same channel and w slice, each of which is
+        appended to the relevant dataset in the file.
+        """
+        buf = self._buffer[:self._used]
+        # mergesort is stable, so will preserve ordering by time
+        buf.sort(kind='mergesort', order=['channel', 'w_slice', 'baseline'])
+        prev_key = None
+        last = None
+        out_pos = 0
+        for element in buf:
+            if element['baseline'] < 0:
+                continue       # Autocorrelation
+            # Here uv and sub_uv are converted to lists because == gives
+            # elementwise results on arrays.
+            key = (element.channel, element.w_slice, element.uv.tolist(), element.sub_uv.tolist(), element.w_plane)
+            if key == prev_key:
+                last.vis[:] += element.vis[:]
+                last.weights[:] += element.weights[:]
+            else:
+                if last is not None:
+                    self._buffer[out_pos] = last
+                    out_pos += 1
+                prev_key = key
+                last = element
+        if last is not None:
+            self._buffer[out_pos] = last
+            out_pos += 1
+        self._used = 0
+        # Write regions to file
+        if out_pos > 0:
+            key = self._buffer[:out_pos][['channel', 'w_slice']]
+            prev = 0
+            for split in np.nonzero(key[1:] != key[:-1])[0]:
+                end = split + 1
+                self._emit(self._buffer[prev:end])
+                prev = end
+            self._emit(self._buffer[prev:out_pos])
+
+    def _emit(self, elements):
+        """Write an array of compressed elements with the same channel and w
+        slice to the backing store. The caller must provide a non-empty
+        array."""
+        raise NotImplementedError()
+
+    def _convert_to_buffer(self, channel, uvw, weights, baselines, vis, out):
+        """Apply element-wise preprocessing to a number of elements and write
+        the results to `out`, which will be a view of a range from the buffer.
+
+        .. todo:: Accelerate with numba
+
+        Parameters
+        ----------
+        channel : int
+            Channel number for all the provided visibilities
+        uvw, weights, baselines, vis : array
+            See :meth:`add`
+        out : record array
+            N-element view of portion of the buffer to write
+        """
+        N = uvw.shape[0]
+        P = vis.shape[1]
+        max_w = self.grid_parameters.w_planes - 1
+        ksize = self.grid_parameters.kernel_width
+        offset = np.float32(self.image_parameters[channel].pixels // 2 - (ksize - 1) // 2)
+        uv_scale = np.float32(1 / self.image_parameters[channel].cell_size)
+        w_scale = float(units.m / self.grid_parameters.max_w) * max_w
+        uvw = uvw.to(units.m).value
+        for row in range(N):
+            u, v, w = uvw[row, :]
+            if w < 0:
+                u = -u
+                v = -v
+                w = -w
+                out[row].vis[:] = np.conj(vis[row, :])
+            else:
+                out[row].vis[:] = vis[row, :]
+            out[row].vis[:] *= weights[row, :]
+            u = u * uv_scale + offset
+            v = v * uv_scale + offset
+            w = np.rint(w * w_scale)
+            w_plane = int(min(w, max_w))
+            w_slice = 0  # TODO: change once w-stacking implemented
+            u0, sub_u = grid.subpixel_coord(u, self.grid_parameters.oversample)
+            v0, sub_v = grid.subpixel_coord(v, self.grid_parameters.oversample)
+            out[row].channel = channel
+            out[row].uv[0] = u0
+            out[row].uv[1] = v0
+            out[row].sub_uv[0] = sub_u
+            out[row].sub_uv[1] = sub_v
+            out[row].w_plane = w_plane
+            out[row].w_slice = w_slice
+            out[row].weights[:] = weights[row, :]
+            out[row].baseline = baselines[row]
+
+    def add(self, channel, uvw, weights, baselines, vis):
+        """Add a set of visibilities to the collector. Each of the provided
+        arrays must have the same size on the first axis.
+
+        Parameters
+        ----------
+        channel : int
+            A channel ID, which indexed the `image_parameters` array passed to
+            the constructor.
+        uvw : Quantity array, Quantity
+            N×3 array of UVW coordinates.
+        weights : array, float32
+            N×P array of weights, where P is the number of polarizations.
+            Flags must be folded into the weights.
+        baselines: array, int
+            1D array of integer, indicating baseline IDs. The IDs are
+            arbitrary and need not be contiguous, and are used only to
+            associate visibilities from the same baseline together.
+            Negative baseline IDs indicate autocorrelations, which will
+            be discarded.
+        vis: array, complex64
+            N×P array of visibilities.
+        """
+        N = uvw.shape[0]
+        if len(uvw.shape) != 2:
+            raise ValueError('uvw has wrong number of dimensions')
+        if len(weights.shape) != 2:
+            raise ValueError('weights has wrong number of dimensions')
+        if len(baselines.shape) != 1:
+            raise ValueError('baselines has wrong number of dimensions')
+        if len(vis.shape) != 2:
+            raise ValueError('vis has wrong number of dimensions')
+        if weights.shape[0] != N or baselines.shape[0] != N or vis.shape[0] != N:
+            raise ValueError('Arrays have different shapes')
+        if uvw.shape[1] != 3:
+            raise ValueError('uvw has invalid shape')
+        ip = self.image_parameters[channel]
+        if weights.shape[1] != len(ip.polarizations):
+            raise ValueError('weights has invalid shape')
+        if vis.shape[1] != len(ip.polarizations):
+            raise ValueError('vis has invalid shape')
+
+        # Copy data to the buffer
+        start = 0
+        while start < N:
+            M = min(N - start, len(self._buffer) - self._used)
+            end = start + M
+            fill = self._buffer[self._used : self._used + M]
+            self._convert_to_buffer(
+                channel,
+                uvw[start : end],
+                weights[start : end],
+                baselines[start : end],
+                vis[start : end], 
+                self._buffer[self._used : self._used + M])
+            self._used += M
+            start += M
+            if self._used == len(self._buffer):
+                self._process_buffer()
+
+    def close(self):
+        """Finalize processing and close any resources. This must be called before
+        :meth:`reader`. Subclasses may overload this method.
+        """
+        self._process_buffer()
+
+    def reader(self):
+        """Create and return a reader object that can be used to iterate over
+        visibility data. This may only be called *after* :meth:`close`.
+        """
+        raise NotImplementedError()
+
+
+class VisibilityCollectorHDF5(VisibilityCollector):
+    """Visibility collector that stores data in an HDF5 file."""
+
+    def __init__(
+            self, filename, *args, **kwargs):
+        super(VisibilityCollectorHDF5, self).__init__(*args, **kwargs)
+        # TODO: need a more intelligent manner to select cache sizes
+        # TODO: investigate adding compression. Should work fairly well on UVW
+        self.filename = filename
+        self.file = h5py.File(h5py.h5f.create(filename, fapl=_make_fapl(100003, 128 * 1024**2)))
+        self.datasets = []
+        for channel in range(self.num_channels):
+            group = self.file.create_group("channel_{}".format(channel))
+            group_datasets = []
+            for w_slice in range(self.num_w_slices):
+                # TODO: set chunk size more sensibly
+                # TODO: ensure that fill is with undefined rather than zeros
+                group_datasets.append(group.create_dataset(
+                    "slice_{}".format(w_slice),
+                    (0,), maxshape=(None,), dtype=self.store_dtype,
+                    chunks=(65536,)))
+            self.datasets.append(group_datasets)
+
+    def _emit(self, elements):
+        N = elements.shape[0]
+        dataset = self.datasets[elements[0].channel][elements[0].w_slice]
+        dataset.resize(dataset.shape[0] + N, axis=0)
+        dataset[-N:] = elements.astype(self.store_dtype)
+
+    def close(self):
+        super(VisibilityCollectorHDF5, self).close()
+        self.file.close()
+        self.file = None
+
+    def reader(self):
+        return VisibilityReaderHDF5(self)
+
+
+class VisibilityCollectorMem(VisibilityCollector):
+    """Visibility collector that stores data in memory. Each dataset is stored
+    as a list of numpy arrays.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(VisibilityCollectorMem, self).__init__(*args, **kwargs)
+        self.datasets = [
+            [ [] for w_slice in range(self.num_w_slices) ] for channel in range(self.num_channels)]
+
+    def _emit(self, elements):
+        dataset = self.datasets[elements[0].channel][elements[0].w_slice]
+        dataset.append(elements.astype(self.store_dtype))
+
+    def reader(self):
+        return VisibilityReaderMem(self)
+
+
+class VisibilityReader(object):
+    """Abstract base class for visibility readers.
+
+    Parameters
+    ----------
+    collector : subclass of :class:`VisibilityCollector`
+        Closed collector that collected the visibilities.
+    """
+    def __init__(self, collector):
+        pass
+
+    def iter_slice(self, channel, w_slice):
+        """A generator that iterates over the visibilities in blocks. Each
+        iteration yields an array of some number of visibilities, in the type
+        returned by :func:`_make_dtype`.
+
+        .. warning:: An implementation may recycle a single buffer for this
+           purpose. The returned array must be consumed (or copied) before
+           requesting the next one.
+        """
+        raise NotImplementedError()
+
+    def len(self, channel, w_slice):
+        """Number of visibilities that will be enumerated by :meth:`iter_slice`"""
+        raise NotImplementedError()
+
+    def close(self):
+        """Free any resources associated with the reader."""
+        pass
+
+
+class VisibilityReaderHDF5(VisibilityReader):
+    def __init__(self, collector):
+        super(VisibilityReaderHDF5, self).__init__(collector)
+        # TODO: think about what to set cache size to
+        self.file = h5py.File(h5py.h5f.open(collector.filename, flags=h5py.h5f.ACC_RDONLY, fapl=_make_fapl(100003, 128 * 1024**2)))
+
+    def _get_dataset(self, channel, w_slice):
+        return self.file['channel_{}'.format(channel)]['slice_{}'.format(w_slice)]
+
+    def iter_slice(self, channel, w_slice):
+        dataset = self._get_dataset(channel, w_slice)
+        block_size = dataset.chunks[0]
+        if block_size is None:
+            block_size = 65536
+        buf = np.empty((block_size,), dataset.dtype)
+        N = dataset.shape[0]
+        for start in xrange(0, N - block_size, block_size):
+            dataset.read_direct(buf, np.s_[start : start+block_size], np.s_[0 : block_size])
+            yield buf
+        last = N % block_size
+        if last > 0:
+            buf = np.empty((last,), dataset.dtype)
+            dataset.read_direct(buf, np.s_[N - last : N], np.s_[0 : last])
+            yield buf
+
+    def len(self, channel, w_slice):
+        return len(self._get_dataset(channel, w_slice))
+
+    def close(self):
+        super(VisibilityReaderHDF5, self).close()
+        self.file.close()
+        self.file = None
+
+
+class VisibilityReaderMem(VisibilityReader):
+    def __init__(self, collector):
+        super(VisibilityReaderMem, self).__init__(collector)
+        self.datasets = collector.datasets
+
+    def iter_slice(self, channel, w_slice):
+        return iter(self.datasets[channel][w_slice])
+
+    def len(self, channel, w_slice):
+        return sum(len(x) for x in self.datasets[channel][w_slice])
+
+    def close(self):
+        super(VisibilityReaderMem, self).close()
+        self.datasets = None
---
+++ katsdpimager/test/test_preprocess.py
@@ -0,0 +1,122 @@
+"""Tests for :py:mod:`katsdpimager.preprocess`."""
+
+from __future__ import print_function, division
+import astropy.units as units
+import numpy as np
+import tempfile
+import logging
+import os
+from contextlib import closing
+from nose.tools import *
+import katsdpimager.parameters as parameters
+import katsdpimager.polarization as polarization
+import katsdpimager.preprocess as preprocess
+
+
+def _empty_recarray(dtype):
+    return np.rec.array(None, dtype=dtype, shape=(0,))
+
+class BaseTestVisibilityCollector(object):
+    def setup(self):
+        self.image_parameters = []
+        for wavelength in np.array([0.25, 0.125]) * units.m:
+            self.image_parameters.append(parameters.ImageParameters(
+                q_fov=1.0,
+                image_oversample=5.0,
+                frequency=wavelength, array=None,
+                polarizations=polarization.STOKES_IQUV,
+                dtype=np.float32,
+                pixel_size=1.0/(4096.0*wavelength.value),
+                pixels=2048))
+        self.grid_parameters = parameters.GridParameters(
+            antialias_width=7.0,
+            oversample=8.0,
+            image_oversample=4.0,
+            w_planes=128,
+            max_w=400 * units.m,
+            kernel_width=64)
+
+    def check(self, collector, expected):
+        reader = collector.reader()
+        for channel, channel_data in enumerate(expected):
+            for w_slice, slice_data in enumerate(channel_data):
+                assert_equal(len(slice_data), reader.len(channel, w_slice))
+                pieces = []
+                for piece in reader.iter_slice(channel, w_slice):
+                    pieces.append(piece.copy())
+                if pieces:
+                    actual = np.rec.array(np.hstack(pieces))
+                else:
+                    actual = np.rec.recarray(0, collector.store_dtype)
+                # np.testing functions don't seem to handle structured arrays
+                # well, so test one field at a time.
+                np.testing.assert_array_equal(actual.uv, slice_data.uv)
+                np.testing.assert_array_equal(actual.sub_uv, slice_data.sub_uv)
+                np.testing.assert_allclose(actual.weights, slice_data.weights)
+                np.testing.assert_allclose(actual.vis, slice_data.vis, rtol=1e-5)
+                np.testing.assert_array_equal(actual.w_plane, slice_data.w_plane)
+
+    def test_empty(self):
+        with closing(self.factory()) as collector:
+            pass
+        self.check(collector, [[np.rec.recarray(0, collector.store_dtype)] for channel in range(2)])
+
+    def test_simple(self):
+        uvw = np.array([
+            [12.1, 2.3, 4.7],
+            [-3.4, 7.6, 2.5],
+            [-5.2, -10.6, 7.2],
+            [12.102, 2.299, 4.6],   # Should merge with the first visibility
+            ], dtype=np.float32) * units.m
+        weights = np.array([
+            [1.3, 0.6, 1.2, 0.0],
+            [1.0, 1.0, 1.0, 1.0],
+            [0.5, 0.6, 0.7, 0.8],
+            [1.1, 1.2, 1.3, 1.4],
+            ], dtype=np.float32)
+        baselines = np.array([
+            0,
+            -1,    # Auto-correlation: should be removed
+            1,
+            0,
+            ], dtype=np.int16)
+        vis = np.array([
+            [0.5 - 2.3j, 0.1 + 4.2j, 0.0 - 3j, 1.5 + 0j],
+            [0.5 - 2.3j, 0.1 + 4.2j, 0.0 - 3j, 1.5 + 0j],
+            [1.5 + 1.3j, 1.1 + 2.7j, 1.0 - 2j, 2.5 + 1j],
+            [1.2 + 3.4j, 5.6 + 7.8j, 9.0 + 1.2j, 3.4 + 5.6j],
+            ], dtype=np.complex64)
+        with closing(self.factory()) as collector:
+            collector.add(0, uvw, weights, baselines, vis)
+        self.check(collector, [[np.rec.fromarrays([
+            [[1089, 1011], [951, 908]],
+            [[6, 3], [3, 1]],
+            [[2.4, 1.8, 2.5, 1.4], [0.5, 0.6, 0.7, 0.8]],
+            [[1.97 + 0.75j, 6.78 + 11.88j, 11.7 - 2.04j, 4.76 + 7.84j],
+             [0.75 + 0.65j, 0.66 + 1.62j, 0.7 - 1.4j, 2.0 + 0.8j]],
+            [1, 2]
+            ], dtype=collector.store_dtype)]])
+
+
+class TestVisibilityCollectorMem(BaseTestVisibilityCollector):
+    def factory(self):
+        return preprocess.VisibilityCollectorMem(self.image_parameters, self.grid_parameters)
+
+class TestVisibilityCollectorHDF5(BaseTestVisibilityCollector):
+    def setup(self):
+        super(TestVisibilityCollectorHDF5, self).setup()
+        self._tmpfiles = []
+
+    def factory(self):
+        handle, filename = tempfile.mkstemp(suffix='.h5')
+        self._tmpfiles.append(filename)
+        os.close(handle)
+        return preprocess.VisibilityCollectorHDF5(filename, self.image_parameters, self.grid_parameters)
+
+    def teardown(self):
+        for filename in self._tmpfiles:
+            try:
+                os.remove(filename)
+            except OSError as e:
+                logging.warning("Failed to remove {}: {}".format(filename, e))
+        self._tmpfiles = []
