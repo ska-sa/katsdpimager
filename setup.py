debbd8de17371db2cc951d3e7f7e9cd2169f406f
Bruce Merry <bmerry@gmail.com>
2015-12-06 11:53:04 +0200

    Split katpoint requirements into separate extra_requires
    It was previously in 'report', but that broke the Jenkins build.

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'test': tests_require,
         'doc': ['sphinx>=1.3.0'],
         'cpu': ['numba'],
-        'report': ['aplpy', 'matplotlib', 'mako', 'katpoint']
+        'report': ['aplpy', 'matplotlib', 'mako'],
+        'benchmark': ['katpoint']
     }
 )
