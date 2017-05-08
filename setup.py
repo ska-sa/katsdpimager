00905c9ec986fdf72405c8fd82554db2473b052b
Bruce Merry <bmerry@ska.ac.za>
2017-05-08 12:30:40 +0200

    Go back to C++11 for better compatibility

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ extensions = [
             get_include('pybind11'),
             get_include('pybind11', user=True)] + list(eigen3.get('include_dirs', [])),
         depends=glob.glob('katsdpimager/*.h'),
-        extra_compile_args=['-std=c++14', '-g0'],
+        extra_compile_args=['-std=c++11', '-g0'],
         libraries=list(eigen3.get('libraries', []))
     )
 ]
