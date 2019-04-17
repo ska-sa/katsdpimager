61750dda11fa34e17e9c1e382f6c86427da7bce9
Bruce Merry <bmerry@ska.ac.za>
2019-04-17 13:38:40 +0200

    Some bytes/str cleanup

--- tests/images_report.py
+++ tests/images_report.py
@@ -39,16 +39,18 @@ class BuildInfo:
         output = io.BytesIO()
         self.returncode = 0
         for cmd in cmds:
+            sys.stdout.flush()
             child = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             # The child.stdout seems to do full reads, even though it is supposedly unbuffered
             stdout = io.open(child.stdout.fileno(), mode='rb', buffering=0, closefd=False)
+            sys_stdout = io.open(sys.stdout.fileno(), mode='wb', buffering=0, closefd=False)
             while True:
                 data = stdout.read(256)
                 if not data:
                     break
                 output.write(data)
-                sys.stdout.write(data)
-                sys.stdout.flush()
+                sys_stdout.write(data)
+                sys_stdout.flush()
             self.returncode = child.wait()
             if self.returncode != 0:
                 logging.warning("Process failed with exit code %s", self.returncode)
