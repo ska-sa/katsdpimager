9f48c78e34ad6309d679744092879db4ddf5d8f0
Bruce Merry <bmerry@ska.ac.za>
2019-06-26 09:43:58 +0200

    Move most of the argument parsing to frontend.py

--- katsdpimager/frontend.py
+++ katsdpimager/frontend.py
@@ -222,6 +222,92 @@ class ChannelParameters:
         log_parameters("CLEAN parameters" + suffix, self.clean_p)
 
 
+def add_options(parser):
+    group = parser.add_argument_group('Input selection')
+    group.add_argument('--input-option', '-i', action='append', default=[], metavar='KEY=VALUE',
+                       help='Backend-specific input parsing option')
+    group.add_argument('--start-channel', '-c', type=int, default=0,
+                       help='Index of first channel to process [%(default)s]')
+    group.add_argument('--stop-channel', '-C', type=int,
+                       help='Index past last channel to process [#channels]')
+    group.add_argument('--subtract', metavar='URL',
+                       help='Sky model with sources to subtract at the start')
+
+    group = parser.add_argument_group('Image options')
+    group.add_argument('--q-fov', type=float, default=1.0,
+                       help='Field of view to image, relative to main lobe of beam [%(default)s]')
+    group.add_argument('--image-oversample', type=float, default=5,
+                       help='Pixels per beam [%(default)s]')
+    group.add_argument('--pixel-size', type=parse_quantity,
+                       help='Size of each image pixel [computed from array]')
+    group.add_argument('--pixels', type=int,
+                       help='Number of pixels in image [computed from array]')
+    group.add_argument('--stokes', type=polarization.parse_stokes, default='I',
+                       help='Stokes parameters to image e.g. IQUV for full-Stokes [%(default)s]')
+    group.add_argument('--precision', choices=['single', 'double'], default='single',
+                       help='Internal floating-point precision [%(default)s]')
+
+    group = parser.add_argument_group('Weighting options')
+    group.add_argument('--weight-type',
+                       choices=[name.lower() for name in weight.WeightType.__members__],
+                       default='natural',
+                       help='Imaging density weights [%(default)s]')
+    group.add_argument('--robustness', type=float, default=0.0,
+                       help='Robustness parameter for --weight-type=robust [%(default)s]')
+
+    group = parser.add_argument_group('Gridding options')
+    group.add_argument('--grid-oversample', type=int, default=8,
+                       help='Oversampling factor for convolution kernels [%(default)s]')
+    group.add_argument('--kernel-image-oversample', type=int, default=4,
+                       help='Oversampling factor for kernel generation [%(default)s]')
+    group.add_argument('--w-slices', type=int,
+                       help='Number of W slices [computed from --kernel-width]')
+    group.add_argument('--w-step', type=parse_quantity, default='1.0',
+                       help='Separation between W planes, in subgrid cells or a distance '
+                            '[%(default)s]'),
+    group.add_argument('--max-w', type=parse_quantity,
+                       help='Largest w, as either distance or wavelengths [longest baseline]')
+    group.add_argument('--aa-width', type=float, default=7,
+                       help='Support of anti-aliasing kernel [%(default)s]')
+    group.add_argument('--kernel-width', type=int, default=60,
+                       help='Support of combined anti-aliasing + w kernel [computed]')
+    group.add_argument('--eps-w', type=float, default=0.001,
+                       help='Level at which to truncate W kernel [%(default)s]')
+
+    group = parser.add_argument_group('Cleaning options')
+    group.add_argument('--psf-cutoff', type=float, default=0.01,
+                       help='fraction of PSF peak at which to truncate PSF [%(default)s]')
+    group.add_argument('--psf-limit', type=float, default=0.5,
+                       help='maximum fraction of image to use for PSF [%(default)s]')
+    group.add_argument('--loop-gain', type=float, default=0.1,
+                       help='Loop gain for cleaning [%(default)s]')
+    group.add_argument('--major-gain', type=float, default=0.85,
+                       help='Fraction of peak to clean in each major cycle [%(default)s]')
+    group.add_argument('--threshold', type=float, default=5.0,
+                       help='CLEAN threshold in sigma [%(default)s]')
+    group.add_argument('--major', type=int, default=1,
+                       help='Major cycles [%(default)s]')
+    group.add_argument('--minor', type=int, default=10000,
+                       help='Max minor cycles per major cycle [%(default)s]')
+    group.add_argument('--border', type=float, default=0.02,
+                       help='CLEAN border as a fraction of image size [%(default)s]')
+    group.add_argument('--clean-mode', choices=['I', 'IQUV'], default='IQUV',
+                       help='Stokes parameters to consider for peak-finding [%(default)s]')
+
+    group = parser.add_argument_group('Performance tuning options')
+    group.add_argument('--vis-block', type=int, default=1048576,
+                       help='Number of visibilities to load and grid at a time [%(default)s]')
+    group.add_argument('--vis-load', type=int, default=32 * 1048576,
+                       help='Number of visibilities to load from file at a time [%(default)s]')
+    group.add_argument('--channel-batch', type=int, default=16,
+                       help='Number of channels to fully process before starting next batch '
+                            '[%(default)s]')
+    group.add_argument('--no-tmp-file', dest='tmp_file', action='store_false', default=True,
+                       help='Keep preprocessed visibilities in memory')
+    group.add_argument('--max-cache-size', type=int, default=None,
+                       help='Limit HDF5 cache size for preprocessing')
+
+
 class Writer:
     """Abstract class that handles writing grids/images to files"""
     @abstractmethod
