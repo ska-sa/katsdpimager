5b04fedea03211be8a481e18f1a292036c15b3a1
Bruce Merry <bmerry@ska.ac.za>
2019-10-07 17:31:59 +0200

    Alternative approach to overriding -std= for wheels
    The previous approach didn't play nice with katversion, because it
    modified the source and hence caused katversion to consider it to be
    dirty. There is now a back-door via an environment variable so that the
    source doesn't need to be changed.

--- manylinux/generate_wheels.sh
+++ manylinux/generate_wheels.sh
@@ -6,7 +6,6 @@ mkdir -p /output
 for d in /opt/python/cp{35,36,37}*; do
     git clean -xdf
     # We know the compiler supports C++17, and using it avoids the need for Boost
-    sed -i 's/-std=c++1y/-std=c++17/' setup.py
-    $d/bin/pip wheel --no-deps .
+    KATSDPIMAGER_STD_CXX=c++17 $d/bin/pip wheel --no-deps .
     auditwheel repair --plat manylinux2010_x86_64 -w /output katsdpimager-*-`basename $d`-linux_*.whl
 done
