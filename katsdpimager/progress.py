e6ead408fafa3b6584b28ce3da14fd20fa99671e
Bruce Merry <bmerry@ska.ac.za>
2020-08-04 14:15:13 +0200

    Update to latest version of progress package
    It makes progress bars implement the context manager protocol, so I was
    able to built my jury-rigged one. It also looks like it's a bit more
    robust about restoring the terminal cursor if the process dies (using an
    atexit handler).

--- katsdpimager/progress.py
+++ katsdpimager/progress.py
@@ -8,7 +8,6 @@ This wraps the :py:mod:`progress` module to add certain enhancements:
 """
 
 import sys
-import functools
 from contextlib import contextmanager
 
 import progress
@@ -54,38 +53,9 @@ def make_progressbar(name, *args, **kwargs):
     return bar
 
 
-class RunOnce:
-    """Wrapper callable that calls the wrapped function only the first time."""
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
-        self.run = False
-
-    def __call__(self, *args, **kwargs):
-        if not self.run:
-            self.run = True
-            self.wrapped(*args, **kwargs)
-
-
-def run_once(func):
-    """Decorator that modifies a function to only run the first time."""
-    wrapper = RunOnce(func)
-    functools.update_wrapper(wrapper, func)
-    return wrapper
-
-
-@contextmanager
-def finishing(bar):
-    """Context manager that ensures that a progress bar is always finished,
-    even if an exception occurs. This avoids cases where the cursor is hidden
-    in the terminal after hitting Ctrl-C."""
-    bar.finish = run_once(bar.finish)
-    yield bar
-    bar.finish()
-
-
 @contextmanager
 def step(name):
     bar = make_progressbar(name, max=1)
-    yield
-    bar.next()
-    bar.finish()
+    with bar:
+        yield
+        bar.next()
